ee03edcd659378ebb29882ac876f64b4e7e4bada
007ravi <mishra007ravi@gmail.com>
2022-04-22 02:17:17 +0530

    Custom Exception Handling on bean validator

--- blog-api/src/main/java/com/personalproject/blogapi/exceptions/GlobalExceptionHandler.java
+++ blog-api/src/main/java/com/personalproject/blogapi/exceptions/GlobalExceptionHandler.java
@@ -3,8 +3,13 @@ package com.personalproject.blogapi.exceptions;
 import com.personalproject.blogapi.payloads.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice//used to handle all exception of controller(contains every imp method to handle exception)
 public class GlobalExceptionHandler {
@@ -14,5 +19,16 @@ public class GlobalExceptionHandler {
 String message=ex.getMessage();
 ApiResponse apiResponse=new ApiResponse(message,false);
 return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleMethodArgsNotValidException(MethodArgumentNotValidException ex){
+Map<String,String>resp=new HashMap<>();
+ex.getBindingResult().getAllErrors().forEach((error)->{
+    String fieldName=((FieldError)error).getField();
+    String message=error.getDefaultMessage();
+    resp.put(fieldName,message);
+});
+    return new ResponseEntity<>(resp,HttpStatus.BAD_REQUEST);
     }
 }

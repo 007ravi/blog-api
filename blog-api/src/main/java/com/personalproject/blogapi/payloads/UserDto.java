83db6f6d3ee928258ab15274d3278ca92638212c
007ravi <mishra007ravi@gmail.com>
2022-04-22 02:16:12 +0530

    Bean Validator or Data validator| user api

--- blog-api/src/main/java/com/personalproject/blogapi/payloads/UserDto.java
+++ blog-api/src/main/java/com/personalproject/blogapi/payloads/UserDto.java
@@ -8,14 +8,28 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.*;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDto {
+
     private int id;
+
+    @NotEmpty//check the balnk validation also
+    @Size(min=4,message = "Username nust be of 4 min characters")
     private String name;
+
+    @Email(message = "Invalid email address")
+    @NotEmpty
     private String email;
+
+    @NotEmpty
+    @Size(min=3,max=8,message = "Password must be 3min character and 8 max character!!")
+  //  @Pattern() to set combination of value which is necessary in password use regex for desired patter
     private String password;
+
+    @NotEmpty
     private String about;
 }
